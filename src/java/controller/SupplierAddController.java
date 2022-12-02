2afbad0371c38ee90550880150d66f01438777b0
dekupac <duchd12b@gmail.com>
2022-12-02 23:33:42 +0700

    add product in supplier control

--- src/java/controller/SupplierAddController.java
+++ src/java/controller/SupplierAddController.java
@@ -6,24 +6,45 @@ package controller;
 
 import dao.CategoryDAO;
 import dao.CityDAO;
+import dao.DeliveryAreaDAO;
+import dao.ProductDAO;
+import dao.ProductHierarchyDAO;
+import dao.ProductImageDAO;
 import dao.SubCategoryDAO;
 import entity.Category;
 import entity.City;
+import entity.DeliveryArea;
 import entity.Product;
+import entity.ProductHierarchy;
+import entity.ProductImage;
 import entity.SubCategory;
 import entity.Supplier;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  *
@@ -42,76 +63,210 @@ public class SupplierAddController extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, FileUploadException, Exception {
         response.setContentType("text/html;charset=UTF-8");
         HttpSession session = request.getSession();
         Supplier supplier = (Supplier) session.getAttribute("supplier");
         int supplierId = supplier.getSupplierId();
-        
+
         String action = request.getParameter("action");
         Connection conn = dbconnect.DBConnect.getConnection();
         CityDAO cityDAO = new CityDAO(conn);
         CategoryDAO cateDAO = new CategoryDAO(conn);
         SubCategoryDAO subCateDAO = new SubCategoryDAO(conn);
-        
+        ProductDAO productDAO = new ProductDAO(conn);
+        DeliveryAreaDAO deliveryAreaDAO = new DeliveryAreaDAO(conn);
+        ProductImageDAO productImageDAO = new ProductImageDAO(conn);
+        ProductHierarchyDAO productHierarchyDAO = new ProductHierarchyDAO(conn);
+
         List<City> allCities = cityDAO.getAllCity();
         List<Category> allCates = cateDAO.getAllCategory();
-        
+
         Map<Category, List<SubCategory>> mapCateSubCate = new LinkedHashMap<>();
-        
+
         for (Category c : allCates) {
             List<SubCategory> subCategorys = subCateDAO.getSubcategoryByCategoryId(c.getCateId());
             mapCateSubCate.put(c, subCategorys);
         }
-        
+
         if (action == null || action.equals("")) {
             request.setAttribute("allCities", allCities);
             request.setAttribute("mapCateSubCate", mapCateSubCate);
             request.getRequestDispatcher("admin-page/supplier-add-product.jsp").forward(request, response);
-            
-        }else if(action.equals("add")){
+
+        } else if (action.equals("add")) {
             // add file anh va file giay phep vao uploads
-            
-            
+            // Create a factory for disk-based file items
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+
+            // Configure a repository (to ensure a secure temp location is used)
+            javax.servlet.ServletContext servletContext = this.getServletConfig().getServletContext();
+            File repository = (File) servletContext.getAttribute("javax.servlet.context.tempdir");
+            factory.setRepository(repository);
+
+            // Create a new file upload handler
+            ServletFileUpload upload = new ServletFileUpload(factory);
+
+            // Parse the request
+            List<FileItem> items = upload.parseRequest(request);
+            // Process the uploaded items
+            Iterator<FileItem> iter = items.iterator();
+
+            List<String> fileImages = new ArrayList<>();
+            String fileLincse = "";
+
+            String description = null;
+            String barcode = null;
+            int subCateId = 0;
+            List<String> cities = new ArrayList<>();
+            String nameP = null;
+            String trademark = null;
+            String taste = null;
+            String color = null;
+            int weight = 0;
+            String packaging = null;
+            String composition = null;
+            int weight1 = 0;
+            int weight2 = 0;
+            int weight3 = 0;
+            int price1 = 0;
+            int price2 = 0;
+            int price3 = 0;
+
+            int number = 0;
+            while (iter.hasNext()) {
+                FileItem item = iter.next();
+                if (!item.isFormField()) {
+                    number++;
+                    String filename = item.getName();
+                    int indexDot = filename.indexOf(".");
+                    SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyyHHmmss");
+                    Date date = new Date();
+                    filename = formatter.format(date) + number + filename.substring(indexDot);
+                    if (filename == null || filename.equals("")) {
+                        break;
+                    } else {
+                        if (item.getFieldName().equals("lincse")) {
+                            fileLincse = filename;
+                        } else {
+                            fileImages.add(filename);
+                        }
+                        Path path = Paths.get(filename);
+                        String storePath = servletContext.getRealPath("/uploads");
+
+                        if (!Files.exists(Paths.get(storePath))) {
+                            Files.createDirectories(Paths.get(storePath));
+                        }
+
+                        File uploadFile = new File(storePath + "/" + path.getFileName());
+                        item.write(uploadFile);
+                    }
+                } else {
+                    String name = item.getFieldName();
+                    String value = item.getString();
+                    if (name.equals("description")) {
+                        description = value;
+                    }
+                    if (name.equals("barcode")) {
+                        barcode = value;
+                    }
+                    if (name.equals("category")) {
+                        subCateId = Integer.parseInt(value);
+                    }
+                    if (name.equals("cities")) {
+                        cities.add(value);
+                    }
+                    if (name.equals("name")) {
+                        nameP = value;
+                    }
+                    if (name.equals("trademark")) {
+                        trademark = value;
+                    }
+                    if (name.equals("taste")) {
+                        taste = value;
+                    }
+                    if (name.equals("color")) {
+                        color = value;
+                    }
+                    if (name.equals("weight")) {
+                        weight = Integer.parseInt(value);
+                    }
+                    if (name.equals("packaging")) {
+                        packaging = value;
+                    }
+                    if (name.equals("composition")) {
+                        composition = value;
+                    }
+                    if (name.equals("weight1")) {
+                        weight1 = Integer.valueOf(value);
+                    }
+                    if (name.equals("weight2")) {
+                        weight2 = Integer.valueOf(value);
+
+                    }
+                    if (name.equals("weight3")) {
+                        weight3 = Integer.valueOf(value);
+
+                    }
+                    if (name.equals("price1")) {
+                        price1 = Integer.valueOf(value);
+
+                    }
+                    if (name.equals("price2")) {
+                        price2 = Integer.valueOf(value);
+
+                    }
+                    if (name.equals("price3")) {
+                        price3 = Integer.valueOf(value);
+
+                    }
+                }
+            }
+
             // add nhung field la field form
-            String description = request.getParameter("description");
-            String barcode = request.getParameter("barcode");
-            String subCate = request.getParameter("category");
-            String[] cities = request.getParameterValues("cities");
-            String name = request.getParameter("name");
-            String trademark = request.getParameter("trademark");
-            String taste = request.getParameter("taste");
-            String color = request.getParameter("color");
-            String weight = request.getParameter("weight");
-            String packaging = request.getParameter("packaging");
-            String composition = request.getParameter("composition");
-            
-            
-            String weight1 = request.getParameter("weight1");
-            String weight2 = request.getParameter("weight2");
-            String weight3 = request.getParameter("weight3");
-            String price1 = request.getParameter("price1");
-            String price2 = request.getParameter("price2");
-            String price3 = request.getParameter("price3");
-            
             Product product = new Product();
             product.setSupplierId(supplierId);
-            product.setSubCateId(Integer.parseInt(subCate));
-            product.setCreatedDate(new Date().toString());
+            product.setSubCateId(subCateId);
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            String dateStr = df.format(new Date());
+            product.setCreatedDate(dateStr);
             product.setDescription(description);
-            product.setProductName(name);
+            product.setProductName(nameP);
             product.setBarCode(barcode);
-            product.setProductCertificate("de trong");
+            product.setProductCertificate(fileLincse);
             product.setTrademark(trademark);
             product.setSmell(taste);
             product.setColor(color);
-            product.setWeight(Integer.parseInt(weight));
+            product.setWeight(weight);
             product.setPacking(packaging);
             product.setElement(composition);
             product.setViewNumber(0);
             product.setStatusId(1);
-            
-            
+            int status = productDAO.saveProduct(product);
+
+            Product productNewest = productDAO.getTop1ProductNewest();
+            int productNewestId = productNewest.getProductId();
+
+            for (String cityId : cities) {
+                DeliveryArea deliveryArea = new DeliveryArea();
+                deliveryArea.setProductId(productNewestId);
+                deliveryArea.setCityId(Integer.parseInt(cityId));
+                deliveryAreaDAO.saveDeliveryArea(deliveryArea);
+            }
+            for (String image : fileImages) {
+                ProductImage productImage = new ProductImage();
+                productImage.setProducId(productNewestId);
+                productImage.setImgPath(image);
+                productImageDAO.saveProductImage(productImage);
+            }
+            ProductHierarchy productHierarchy1 = new ProductHierarchy(productNewestId, weight1, price1);
+            ProductHierarchy productHierarchy2 = new ProductHierarchy(productNewestId, weight2, price2);
+            ProductHierarchy productHierarchy3 = new ProductHierarchy(productNewestId, weight3, price3);
+            productHierarchyDAO.saveProductHierarchy(productHierarchy1);
+            productHierarchyDAO.saveProductHierarchy(productHierarchy2);
+            productHierarchyDAO.saveProductHierarchy(productHierarchy3);
+            response.sendRedirect("SupplierController");
+
         }
     }
 
@@ -127,7 +282,13 @@ public class SupplierAddController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (FileUploadException ex) {
+            Logger.getLogger(SupplierAddController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(SupplierAddController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -141,7 +302,13 @@ public class SupplierAddController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (FileUploadException ex) {
+            Logger.getLogger(SupplierAddController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(SupplierAddController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
--- src/java/dao/ProductDAO.java
+++ src/java/dao/ProductDAO.java
@@ -354,32 +354,18 @@ public class ProductDAO {
         return product == null ? 0 : 1;
     }
 
-    public List<Product> searchProduct(String idStr, String nameStr, String barcodeStr) {
+    public List<Product> searchProduct(int SupplierID, String idStr, String nameStr, String barcodeStr) {
         List<Product> products = new ArrayList<>();
-        String strSearch = "select * from Product";
+        String strSearch = "select * from Product where SupplierID="+SupplierID;
 
-        
-        boolean checkStart = true;
-        if ((idStr != null && !idStr.trim().isEmpty()) || (nameStr != null && !nameStr.trim().isEmpty()) 
-                || (barcodeStr != null && !barcodeStr.trim().isEmpty())) {
-            strSearch += " where";
-        }
         if (idStr != null && !idStr.trim().isEmpty()) {
-            strSearch += " ProductID = " + idStr;
-            checkStart = false;
+            strSearch += "and ProductID = " + idStr;
         }
         if (nameStr != null && !nameStr.trim().isEmpty()) {
-            if (checkStart == false) {
-                strSearch += " and";
-            }
-            strSearch += " ProductName like N'%" + nameStr + "%'";
-            checkStart = false;
+            strSearch += "and ProductName like N'%" + nameStr + "%'";
         }
         if (barcodeStr != null && !barcodeStr.trim().isEmpty()) {
-            if (checkStart == false) {
-                strSearch += " and";
-            }
-            strSearch += " BarCode like '%" + barcodeStr + "%'";
+            strSearch += "and BarCode like '%" + barcodeStr + "%'";
         }
         System.out.println(strSearch);
         try {
@@ -442,5 +428,36 @@ public class ProductDAO {
             System.out.println(e.getMessage());
         }
         return products;
+    }
+
+    public Product getTop1ProductNewest() {
+        Product product = null;
+        String strSelectById = "select top(1) * from product order by productid desc";
+        try {
+            PreparedStatement ps = connection.prepareStatement(strSelectById);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                product = new Product();
+                product.setProductId(rs.getInt("ProductID"));
+                product.setSupplierId(rs.getInt("SupplierID"));
+                product.setSubCateId(rs.getInt("SubCateID"));
+                product.setCreatedDate(rs.getString("CreatedDate"));
+                product.setDescription(rs.getString("Description"));
+                product.setProductName(rs.getString("ProductName"));
+                product.setBarCode(rs.getString("BarCode"));
+                product.setProductCertificate(rs.getString("ProductCertificate"));
+                product.setTrademark(rs.getString("Trademark"));
+                product.setSmell(rs.getString("Smell"));
+                product.setColor(rs.getString("Color"));
+                product.setWeight(rs.getInt("Weight"));
+                product.setPacking(rs.getString("Packing"));
+                product.setElement(rs.getString("Element"));
+                product.setViewNumber(rs.getInt("ViewNumber"));
+                product.setStatusId(rs.getInt("StatusID"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return product;
     }
 }
